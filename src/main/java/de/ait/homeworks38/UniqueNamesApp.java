1ca10fcd4850873281c1d71463665c917bbbc1b4
angelShlesser <angelShlesser2010@gmail.com>
2023-10-21 13:33:49 +0200

    Update Lessons 38

--- src/main/java/de/ait/homeworks38/UniqueNamesApp.java
+++ src/main/java/de/ait/homeworks38/UniqueNamesApp.java
@@ -4,26 +4,46 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 public class UniqueNamesApp {
     public static void main(String[] args) {
         Set<String> nameList = new HashSet<>();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите имена (или введите 'стоп' для завершения):");
-        while (true){
+        while (true) {
             String input = scanner.nextLine().trim();
-            if (input.equals("стоп")){
+            if (input.equalsIgnoreCase("стоп")) {
                 break;
             }
-            nameList.add(input);
+            // Проверка на отсутствие цифр в имени
+            if (!containsDigits(input)) {
+                if (nameList.add(input)) {
+                    System.out.println("Имя '" + input + "' добавлено.");
+                } else {
+                    System.out.println("Имя '" + input + "' не было добавлено, так как оно уже существует.");
+                }
+            } else {
+                System.out.println("Имя не должно содержать цифры. Попробуйте снова.");
+            }
         }
         scanner.close();
 
         System.out.println("---------");
         System.out.println("Уникальные имена:");
-        Iterator<String> iterator = nameList.iterator();
-        while (iterator.hasNext()){
-            System.out.println(iterator.next());
+
+        if (nameList.isEmpty()) {
+            System.out.println("Список имен пуст.");
+        } else {
+            Iterator<String> iterator = nameList.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
         }
+    }
+
+    // Метод для проверки наличия цифр в строке
+    private static boolean containsDigits(String s) {
+        return Pattern.compile("[0-9]").matcher(s).find();
     }
 }
