8e10a885780f6f3d0fcccdffd2d964016d2d8c70
angelShlesser <angelShlesser2010@gmail.com>
2023-11-04 18:05:56 +0100

    add Lessons 45

---
+++ src/main/java/de/ait/homeworks45/SafeArrayElementExtraction.java
@@ -0,0 +1,48 @@
+package de.ait.homeworks45;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
+
+public class SafeArrayElementExtraction {
+    private static final Logger log = LoggerFactory.getLogger(SafeArrayElementExtraction.class);
+    public static void main(String[] args) {
+        String[] array = {"Apple", "Banana", "Cherry", "Date", "Fig"};
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Введите индекс элемента для извлечения: ");
+
+            try {
+                int index = Integer.parseInt(scanner.nextLine());
+                String element = safeExtractElement(array, index);
+
+                if (element != null) {
+                    log.info("Извлеченный элемент: " + element);
+                }
+            } catch (NumberFormatException exception) {
+                log.error("Не верный формат индекса массива");
+            } finally {
+                log.info("Операция поиска завершена.");
+            }
+        }
+    }
+
+    public static String safeExtractElement(String[] array, int index) {
+        if (array == null) {
+            log.info("Массив отсутствует.");
+            return null;
+        }
+
+        try {
+            if (index < 0 || index >= array.length) {
+                throw new ArrayIndexOutOfBoundsException();
+            }
+            return array[index];
+        } catch (ArrayIndexOutOfBoundsException exception) {
+            log.warn("Введено число за пределами массива");
+        }
+        return null;
+    }
+}
