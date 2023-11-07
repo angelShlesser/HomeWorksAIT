f8574405b1c647f447d65f260c3f245234445295
angelShlesser <angelShlesser2010@gmail.com>
2023-11-07 19:27:03 +0100

    Update Lessons 45

--- src/main/java/de/ait/homeworks45/SafeArrayElementExtraction.java
+++ src/main/java/de/ait/homeworks45/SafeArrayElementExtraction.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class SafeArrayElementExtraction {
     private static final Logger LOGGER = LoggerFactory.getLogger(SafeArrayElementExtraction.class);
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
     private static String[] array;
 
     public static void main(String[] args) {
@@ -15,26 +15,9 @@ public class SafeArrayElementExtraction {
         scanner.close();
     }
 
-    public static String safeExtractElement(String[] array, int index) {
-        if (array == null) {
-            LOGGER.info("Массив отсутствует.");
-            return null;
-        }
-
-        try {
-            if (index < 0 || index >= array.length) {
-                throw new ArrayIndexOutOfBoundsException();
-            }
-            return array[index];
-        } catch (ArrayIndexOutOfBoundsException exception) {
-            LOGGER.warn("Введено число за пределами массива");
-        }
-        return null;
-    }
-
     public static void processUserInput() {
         while (true) {
-            System.out.print("Введите элемента для извлечения от 1 до 5 (или 'exit' для завершения): ");
+            LOGGER.info("Введите элемента для извлечения от 1 до 5 (или 'exit' для завершения): ");
             String input = scanner.nextLine();
 
             if (input.equals("exit")) {
@@ -45,11 +28,13 @@ public class SafeArrayElementExtraction {
             try {
                 int result = Integer.parseInt(input);
                 int index = result - 1;
-                String element = safeExtractElement(array, index);
+                String element = array[index];
 
                 if (element != null) {
                     LOGGER.info("Извлеченный элемент: {}", element);
                 }
+            } catch (ArrayIndexOutOfBoundsException exception) {
+                LOGGER.error("ERROR. Введено число за пределами массива");
             } catch (NumberFormatException exception) {
                 LOGGER.error("Не верный формат индекса массива");
             } finally {
@@ -60,7 +45,7 @@ public class SafeArrayElementExtraction {
 
     public static void createAndPopulateArray() {
         array = new String[5];
-        System.out.println("Заполните массив (5 элементов):");
+        LOGGER.info("Заполните массив (5 элементов):");
         for (int i = 0; i < array.length; i++) {
             array[i] = scanner.nextLine();
             LOGGER.info("Добавлен {} элемент массива: {}", i + 1, array[i]);
