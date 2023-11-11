e23d994c357726b8c038421cd22b9219f867b742
angelShlesser <angelShlesser2010@gmail.com>
2023-11-11 10:02:41 +0100

    Update Lessons 46

--- src/main/java/de/ait/homeworks46/UserRegistration.java
+++ src/main/java/de/ait/homeworks46/UserRegistration.java
@@ -4,95 +4,62 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Scanner;
-import java.util.regex.PatternSyntaxException;
 
 public class UserRegistration {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserRegistration.class);
 
     public static void main(String[] args) {
+        boolean isRegistrationSuccessful = false;
 
-        boolean validateResult = false;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Введите имя пользователя или 'exit' для выхода:");
+            String username = scanner.nextLine();
 
-        System.out.println("Введите имя пользователя или 'exit' для выхода:");
-
-        Scanner scanner = new Scanner(System.in);
-        String username = null;
-        boolean usernameValide = false;
-        while (!validateResult) {
-            try {
-                if (!usernameValide) {
-                    username = scanner.nextLine();
-                    username.charAt(0);
-                    System.out.println("Username: " + username);
-                    usernameValide = true;
-                    if (username.equals("exit")) {
-                        break;
-                    }
-                }
-                System.out.println("Введите пароль: ");
-                String password = scanner.next();
-                boolean result = validatePassword(password);
-                // boolean result = isPasswordValid(password);
-                if (result) {
-                    System.out.println("Учётная запись успешно создана.");
-                    LOGGER.info("User account was created {}", username);
-                    validateResult = result;
-                } else {
-                    System.out.println("Введите пароль: ");
-                    password = scanner.next();
-                    validateResult = validatePassword(password);
-                    if (validateResult) {
-                        System.out.println("Учётная запись успешно создана.");
-                        LOGGER.info("User account was created {}", username);
-
-                    }
-                }
-            } catch (IndexOutOfBoundsException exception) {
-                System.out.println("Имя пользователя не может быть пустым ");
-                LOGGER.error("Username ist empty", exception);
-                System.out.println("Введите имя пользователя или 'exit' для выхода:");
+            if (!username.equals("exit")) {
+                isRegistrationSuccessful = registerUser(username, scanner);
             }
 
-        }
-        scanner.close();
-    }
-
-    private static boolean validatePassword(String password) {
-        // !password.matches("\\") ведет к PatternSyntaxException
-
-        try {
-            if (password.length() < 8 || !(password.contains("@")
-                    || password.contains("!") || password.contains("#"))
-                    || !password.matches(".*\\d.*")) {
-                throw new WrongArgumentsException("Password length must be 8 symbols and include" +
-                        " one number and include one special character ");
-            } else {
-                return true;
-            }
-        } catch (WrongArgumentsException | PatternSyntaxException exception) {
-            LOGGER.error("Password is wrong:  {}", password, exception);
-            System.out.println("Ошибка: Пароль должен быть не менее 8 символов и " +
-                    "содержать хотя бы одну цифру и один специальный символ.");
-            return false;
+        } catch (Exception e) {
+            LOGGER.error("Произошла ошибка", e);
         }
 
+        if (isRegistrationSuccessful) {
+            System.out.println("Учётная запись успешно создана.");
+        }
     }
 
-    public static boolean isPasswordValid(String password) {
+    private static boolean registerUser(String username, Scanner scanner) {
         try {
-            System.out.println("Вы зашли");
-            if (password.length() < 8) {
-                throw new ArithmeticException("Вы ушли");
+            if (username.isEmpty()) {
+                throw new IllegalArgumentException("Имя пользователя не может быть пустым.");
             }
-            boolean res = (password.contains("@") || password.contains("!") || password.contains("#"));
-            System.out.println(res);
-            return res;
-        } catch (ArithmeticException exception) {
-            LOGGER.info("Неверный пароль, повторите попытку");
+
+            System.out.println("Введите пароль:");
+            String password = scanner.nextLine();
+            validatePassword(password);
+
+            LOGGER.info("Успешная регистрация пользователя: {}", username);
+            return true;
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+            LOGGER.error("Ошибка при регистрации пользователя: {}", username, e);
             return false;
         }
     }
 
+    private static void validatePassword(String password) {
+        if (password.length() < 8 || !containsCharacterCategory(password)) {
+            throw new IllegalArgumentException("Пароль должен быть не менее 8 символов и содержать хотя бы " +
+                    "по одному символу из каждой категории: верхний регистр, нижний регистр, цифры и специальные символы.");
+        }
+    }
+
+    private static boolean containsCharacterCategory(String str) {
+        return str.matches(".*[A-Z].*") && // хотя бы один символ в верхнем регистре
+                str.matches(".*[a-z].*") && // хотя бы один символ в нижнем регистре
+                str.matches(".*\\d.*") &&   // хотя бы одна цифра
+                str.matches(".*[@!#].*");   // хотя бы один специальный символ
+    }
 }
-
