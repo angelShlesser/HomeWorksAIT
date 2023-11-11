c5cfed3270b789c1eaaee456eb710ccaf0df3190
angelShlesser <angelShlesser2010@gmail.com>
2023-11-11 09:56:54 +0100

    Update Lessons 47

--- src/main/java/de/ait/homeworks47/CurrencyConverter.java
+++ src/main/java/de/ait/homeworks47/CurrencyConverter.java
@@ -21,26 +21,31 @@ public class CurrencyConverter {
         String core;
         double result;
 
-        do {
+        while (true) {
             System.out.println("Введите валюту для конвертации (например, usd, euro):");
             core = scanner.nextLine().toLowerCase();
 
-            switch (core) {
-                case "usd":
-                    result = amount * USD;
-                    break;
-                case "euro":
-                    result = amount * EURO;
-                    break;
-                default:
-                    LOGGER.error("Некорректный код валюты: {}", core);
-                    System.out.println("Некорректный код валюты. Повторите ввод.");
-                    continue; // Повторяем цикл при дефолтном варианте
-            }
+            try {
+                switch (core) {
+                    case "usd":
+                        result = amount * USD;
+                        break;
+                    case "euro":
+                        result = amount * EURO;
+                        break;
+                    default:
+                        LOGGER.error("Некорректный код валюты: {}", core);
+                        throw new IllegalArgumentException("Некорректный код валюты: " + core);
+                }
 
-            LOGGER.info("Выполнена конвертация: {}", result);
-            System.out.println("Конвертация: " + result);
-        } while (false); // Можно использовать другое условие для повторного ввода
+                LOGGER.info("Выполнена конвертация: {}", result);
+                System.out.println("Конвертация: " + result);
+                break; // Выходим из цикла после успешной конвертации
+
+            } catch (IllegalArgumentException e) {
+                System.out.println("Ошибка: " + e.getMessage());
+            }
+        }
     }
 
     private static double scannerUser() {
@@ -53,8 +58,10 @@ public class CurrencyConverter {
                 result = scanner.nextDouble();
                 scanner.nextLine(); // Очистка буфера ввода
                 break; // Выходим из цикла при успешном вводе
+
             } catch (InputMismatchException exception) {
                 LOGGER.error("Ошибка ввода. Пожалуйста, введите числовое значение.", exception);
+                System.out.println("Ошибка ввода. Пожалуйста, введите числовое значение.");
                 scanner.nextLine(); // Очистка буфера ввода
             }
         }
