8f19de34ef9eb8584c74c115da32536d6d9ad360
angelShlesser <angelShlesser2010@gmail.com>
2023-11-25 09:49:38 +0100

    Add Lessons 52

---
+++ src/test/java/de/ait/homeworks52/AirportFileCreator.java
@@ -0,0 +1,65 @@
+package de.ait.homeworks52;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class AirportFileCreator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AirportFileCreator.class);
+
+    public static void main(String[] args) {
+        File file = new File("files/airports.txt");
+
+        if (!file.exists()) {
+            try {
+                // Создание всех несуществующих директорий
+                file.getParentFile().mkdirs();
+
+                if (file.createNewFile()) {
+                    LOGGER.info("Успешное создание файла: {}", file.getAbsolutePath());
+                } else {
+                    LOGGER.error("Не удалось создать файл.");
+                }
+            } catch (IOException e) {
+                LOGGER.error("Ошибка при создании файла: {}", e.getMessage());
+            }
+        } else {
+            LOGGER.warn("Файл уже существует: {}", file.getAbsolutePath());
+        }
+
+        // Информация об аэропортах
+        String[] airportData = {
+                "Paris-Charles-de-Gaulle CDG France",
+                "Heathrow Airport LHR United Kingdom",
+                "Los Angeles International Airport LAX United States",
+                "Tokyo Haneda Airport HND Japan",
+                "Sydney Airport SYD Australia"
+        };
+
+        // Вызов метода для создания файла с информацией об аэропортах
+        createAirportFile(file, airportData);
+    }
+
+    /**
+     * Создает файл с информацией об аэропортах.
+     *
+     * @param filePath    Путь к файлу.
+     * @param airportData Массив строк с информацией об аэропортах.
+     */
+    public static void createAirportFile(File filePath, String[] airportData) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            for (String data : airportData) {
+                writer.write(data);
+                writer.newLine();
+                LOGGER.info("Информация записана: {}", data);
+            }
+            LOGGER.info("Информация успешно записана в файл: {}", filePath);
+        } catch (IOException e) {
+            LOGGER.error("Ошибка при создании файла: {}", e.getMessage());
+        }
+    }
+}
---
+++ src/test/java/de/ait/homeworks52/AirportFileReader.java
@@ -0,0 +1,58 @@
+package de.ait.homeworks52;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class AirportFileReader {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AirportFileReader.class);
+
+    public static void main(String[] args) {
+        File file = new File("files/airports.txt");
+
+        if (file.exists()) {
+            try (
+                    // Создание BufferedReader для чтения из файла
+                    BufferedReader reader = new BufferedReader(new FileReader(file));
+                    ){
+
+                // Чтение строк из файла
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Разделение строки на части
+                    String[] parts = line.split("\\s+");
+
+                    if (parts.length >= 3) {
+                        // Вывод информации об аэропорте в консоль
+                        String airportName = parts[0];
+                        String iataCode = parts[1];
+                        String country = parts[2];
+
+                        printAirportInfo(airportName, iataCode, country);
+                    } else {
+                        LOGGER.warn("Неверный формат строки в файле: {}", line);
+                    }
+                }
+            } catch (IOException e) {
+                LOGGER.error("Ошибка при чтении файла: {}", e.getMessage());
+            }
+        } else {
+            LOGGER.error("Файл не существует: {}", file.getAbsolutePath());
+        }
+    }
+
+    /**
+     * Вывод информации об аэропорте в консоль.
+     *
+     * @param airportName Название аэропорта.
+     * @param iataCode    Код IATA.
+     * @param country     Страна.
+     */
+    private static void printAirportInfo(String airportName, String iataCode, String country) {
+        System.out.println("Название: " + airportName + ", Код IATA: " + iataCode + ", Страна: " + country);
+    }
+}
